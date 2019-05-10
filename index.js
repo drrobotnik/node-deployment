7c83956d3da19380044910636d9cf40cf00270c5
drrobotnik <brandon.lavigne@gmail.com>
2019-05-10 10:51:20 -0700

    rename downstream job to be more clear

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const slog = require('./lib/slog/slog');
 
 const {
 	APP_PATH,
-	DOWNSTREAM_JOB_PATH,
+	DOWNSTREAM_JOB,
 	REPO_NAME,
 	SECRET,
 	SERVER_PORT,
@@ -36,7 +36,7 @@ webhooks.on('pull_request', ({payload}) => {
 	// output the status of the PR.
 	slog( `Pull Request ${action}, ${pull_request.merged}` );
 	if( action === 'closed' && pull_request.merged === true ) {
-		deploy(payload, APP_PATH, DOWNSTREAM_JOB_PATH);
+		deploy(payload, APP_PATH, DOWNSTREAM_JOB);
 	}
 });
 
